11dcb37ac06e46f63cc74a0e8c0fe216b84fe366
EdgardoMD <60943753+EdgardoMD@users.noreply.github.com>
2020-10-17 19:52:32 -0300

    Se agregan interfaces a repo y a service

---
+++ sb-escalab-tarea/src/main/java/com/sb/escalab/tarea/repo/ICompraProductoRepo.java
@@ -0,0 +1,9 @@
+package com.sb.escalab.tarea.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sb.escalab.tarea.model.CompraProducto;
+
+public interface ICompraProductoRepo extends JpaRepository<CompraProducto, Integer> {
+
+}
